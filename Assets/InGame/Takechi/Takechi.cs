59270aeab5af4dff6d714271d0da9e3a16d34dbf
NakamuraTakuto <rulitaku@icloud.com>
2023-12-16 12:59:12 +0900

    [Add] DOTweenとCinemachinの追加 / 各種作業フォルダの作成

---
+++ Assets/InGame/Takechi/Takechi.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Takechi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
