a9aec32662daaacb73727a74874a23d7e10a49e5
NakamuraTakuto <rulitaku@icloud.com>
2023-12-19 12:13:57 +0900

    [Add] シーン遷移用のSceneChangerの作成

--- Assets/InGame/Nakamura/Nakamura.cs
+++ Assets/InGame/Nakamura/Nakamura.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Nakamura : MonoBehaviour
 {
+    Rigidbody2D _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            _rb.AddForce(Vector2.up * 100, ForceMode2D.Impulse);
+        }
     }
 }
