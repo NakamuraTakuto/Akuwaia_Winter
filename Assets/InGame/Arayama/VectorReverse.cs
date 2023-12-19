508631992cd79421e21eed482a9ef80a80dfafc3
ArayamaMiu <134475015+ArayamaMiu@users.noreply.github.com>
2023-12-19 14:02:01 +0900

    [add]砂時計の機能の実装

---
+++ Assets/InGame/Arayama/VectorReverse.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using Takechi.InGame;
+using UnityEngine;
+
+public class VectorReverse : MonoBehaviour
+{
+    [SerializeField] public float _reverseTime = 5f;
+    IEnumerator Reverse(Player player)
+    {
+        Debug.Log("d—Í‚ª”½“]‚µ‚½");
+        player.Hourglass = true;
+        yield return new WaitForSeconds(_reverseTime);
+        player.Hourglass = false;
+        Debug.Log("d—Í‚ª–ß‚Á‚½");
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var player = collision.GetComponent<Player>();
+            if (player)
+            {
+                StartCoroutine(Reverse(player));
+                Debug.Log("»Œv");
+            }
+        }
+    }
+}
+
